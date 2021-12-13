c5a5d001b66426d609e4735d23cf508a0ed22571
SamirRodrigues <emaildesamir@gmail.com>
2021-12-13 18:58:43 -0300

    deploy

---
+++ build/precache-manifest.0cf72c503a72aa8abac5f941a42ff5bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41a89368f81de2d9d42c349ea8d6dc3",
+    "url": "/NavBar/index.html"
+  },
+  {
+    "revision": "c7e36d3a5d5c31bc5bea",
+    "url": "/NavBar/static/css/main.beaf029b.chunk.css"
+  },
+  {
+    "revision": "9a0f8b73cfb6a8343e60",
+    "url": "/NavBar/static/js/2.81d3b123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NavBar/static/js/2.81d3b123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e36d3a5d5c31bc5bea",
+    "url": "/NavBar/static/js/main.c8065363.chunk.js"
+  },
+  {
+    "revision": "1942ebc988f84e852c77",
+    "url": "/NavBar/static/js/runtime-main.181f84dd.js"
+  },
+  {
+    "revision": "2bb7da653fb63433560ef10511c9ae9d",
+    "url": "/NavBar/static/media/logo.2bb7da65.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NavBar/precache-manifest.0cf72c503a72aa8abac5f941a42ff5bd.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NavBar/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
